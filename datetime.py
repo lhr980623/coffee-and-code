b0871a5bce0dc327f50dde3f75e812bb519507c6
Henry <Henry@UserdeMacBook-Pro.local>
2019-07-02 16:28:54 -0400

    this is my first repo for coffee

---
+++ datetime.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('This is coffee and code')
+
+import datetime
+now = datetime.datetime.now()
+print('the time now is:', now)
